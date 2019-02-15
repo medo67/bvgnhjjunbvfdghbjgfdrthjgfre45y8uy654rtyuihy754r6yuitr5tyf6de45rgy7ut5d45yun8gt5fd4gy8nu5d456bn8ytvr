1a42669cff72a964011b610e716c16bb4811ea65
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-15 18:17:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "m";
-(message.content.startsWith(adminprefix + 'st')) 
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
- }
+
+
+   console.log(`----------------`);
+      console.log(`Joker Bot- Script By: Jack NV `);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Best Life`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd"))
 });
-
-
-
-client.login(process.env.BOT_TOKEN);
