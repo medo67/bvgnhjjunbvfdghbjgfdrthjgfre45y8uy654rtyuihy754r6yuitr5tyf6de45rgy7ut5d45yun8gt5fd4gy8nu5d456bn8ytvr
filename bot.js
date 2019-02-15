0a88f2275d57cc2b2db92d0bf08b0d877b3f608d
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-15 18:10:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "m";
-(message.content.startsWith(adminprefix + 'st')) {
+(message.content.startsWith(adminprefix + 'st')) 
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
       message.channel.send(`**✅**`)
   }
